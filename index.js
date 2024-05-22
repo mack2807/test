9a05a7694c724f36c5c3933eb13f52a95fd5c8ff
vnay1507 <vnay1507@gmail.com>
2024-05-22 08:26:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express= require('express')
+const app= express();
+
+const PORT= 8080;
+
+app.get(('/'),(req,res)=>{
+res.send('hello world')
+})
+
+app.listen(PORT,()=>{
+    console.log(`server running on PORT ${PORT}`);
+})
